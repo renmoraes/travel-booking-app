b315558e6d9b191eba73c91f446c8ef9e79807f7
Renan <renanipb@hotmail.com>
2023-02-21 22:54:43 +0000

    Initial commit for the bookings microservice

---
+++ bookings/src/test/java/com/tus/booking/BookingsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tus.booking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
